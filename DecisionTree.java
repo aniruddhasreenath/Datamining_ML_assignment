d759300ef30f4dcb22133600d9d641e560d0abd5
aniruddhasreenath <as17@illinois.edu>
2016-11-23 18:00:56 -0600

    DT

--- DecisionTree.java
+++ DecisionTree.java
@@ -1,10 +1,22 @@
+import java.util.ArrayList;
+
 /**
  * Created by as17 on 11/18/16.
  */
 public class DecisionTree {
 
+    public ArrayList<Sample> sampleSpace;
+
+    public DecisionTree(ArrayList<Sample> samples){
+        sampleSpace = new ArrayList<Sample>();
+
+        sampleSpace = samples;
+    }
+
+    public void calculateGiniIndex(){
 
 
 
+    }
 
 }
